c8cfd276a5eb79384d493512efcad3b3e608a7f0
Paulo Moraes Pessoa Jardim <paulompj010@gmail.com>
2024-03-28 14:13:03 -0300

    Aula3-strings, textos

---
+++ 3_Str_tipo_de_dados.py
@@ -0,0 +1,10 @@
+#strings sao textos que estao dentro de aspas "" ou ''
+print("Paulo")
+
+#Colocando aspas dentro de uma string
+#-Usando caractere de escape
+print("\"Paulo\"")
+
+#                       TRUQUE DO PROFESSOR
+#Se quiser imprimir aspas duplas na tela, coloque aspas simples no print. Dessa maneira:
+print("'Palestra Mário'")
