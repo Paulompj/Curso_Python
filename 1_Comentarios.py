e5eabd7110523005b7831142c1710b84f927ae86
Paulo Moraes Pessoa Jardim <paulompj010@gmail.com>
2024-03-28 14:11:22 -0300

    Aula1- Comentários em python

---
+++ 1_Comentarios.py
@@ -0,0 +1,12 @@
+
+"""
+ISSO NAO É UM COMENTÁRIO.
+É UMA DOCSTRING
+DIFERENTE DO COMENTÁRIO, UMA DOCSTRING É LIDA PELO COMPILADOR
+
+"""
+'''
+PODE USAR 3 ASPAS SIMPLES OU DUPLAS
+'''
+#comentários em uma linha usa o HASHTAG
+#Isso é um comentário em python
