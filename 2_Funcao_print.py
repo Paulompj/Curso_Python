c83df29f5f32a1a84f72ca035a24896997723a98
Paulo Moraes Pessoa Jardim <paulompj010@gmail.com>
2024-03-28 14:12:03 -0300

    Aula2- Função print

---
+++ 2_Funcao_print.py
@@ -0,0 +1,15 @@
+#imprimindo apenas numeros com print
+print(21,22,121)
+print(21,22,121)
+
+#O Modificador abaixo sep serve para colocar o que voce quiser entre os objetos
+#Por padrão ele ja vem com sep=' ' ou seja um espaço em branco
+print(21,22,121,sep="-")
+print(21,22,121,sep='-')
+
+#O modificador end abaixo serve para modificar o ultimo caractere do print
+#Por padrão, ele vem com um \n. Tanto é que diferente do C, o print do python ja pula uma linha sozinho
+print(21,22,121,end='eu')
+print(21,22,121, end='\n')
+print(21,22,121)
+
